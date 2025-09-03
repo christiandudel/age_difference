6671b721200069b43731c2f4ef736332da453f17
Dudel <dudel@demogr.mpg.de>
2025-09-03 14:53:32 +0200

    Updated simulations

--- Code/simulations.R
+++ Code/simulations.R
@@ -1,9 +1,11 @@
-intercept <- 0.07
-mage <- 0.00015
-fage <- 0.00030
+### Linear probability model 
 
-sizes <- seq(50,2500,by=5)
-replications <- 500
+intercept <- 0.07
+mage <- 0.0005
+fage <- 0.00075
+
+sizes <- seq(50,25000,by=50)
+replications <- 100
 
 result1 <- numeric(length(sizes))
 result2 <- numeric(length(sizes))
@@ -23,12 +25,14 @@ for(size in sizes) {
       dat <- data.frame(agem=agem,
                         agef=agef)
       
-      dat$predicted <- intercept + dat$agem*mage + dat$agef*fage + abs(rnorm(sd=0.02,n=size))
+      dat$predicted <- intercept + dat$agem*mage + dat$agef*fage + abs(rnorm(sd=0.01,n=size))
+      dat$prob <- runif(n=size)
+      dat$outcome <- as.numeric(dat$predicted>=dat$prob)
       
       dat$diff <- dat$agem - dat$agef
       
-      fit1 <- lm(predicted~agem+agef,data=dat)
-      fit2 <- lm(predicted~agem+diff,data=dat)
+      fit1 <- lm(outcome~agem+agef,data=dat)
+      fit2 <- lm(outcome~agem+diff,data=dat)
   
       tmp1[rep] <- (summary(fit1))$coefficients["agem",4]
       tmp2[rep] <- (summary(fit2))$coefficients["agem",4]
